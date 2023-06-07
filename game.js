e3a831ae3f4d28e21a5c80c60142abd023de62ea
ElizaG00d <gooden.laurel.232@outlook.com>
2023-06-06 23:24:16 -0500

    sqaure in the bounds

--- game.js
+++ game.js
@@ -42,6 +42,35 @@ const controller = {
 //loop animation
 const loop = () => {
     if (controller.up && square.jumping == false) {
-        
+        square.yVelocity -= 20;
+        square.jumping = true;
     }
+    if (controller.left) {
+        square.xVelocity -= 0.5;
+    }
+    if (controller.right) {
+        square.xVelocity += 0.5;
+    }
+
+    square.yVelocity += 1.5; //gravity
+    square.x += square.xVelocity;
+    square.y += square.yVelocity;
+    square.xVelocity *= 0.9; //friction
+    square.yVelocity *= 0.9; //friction
+
+    //if square below floor line
+    if (square.y > 386 - 16 - 32) {
+        square.jumping = false;
+        square.y = 386 - 16 - 32;
+        square.yVelocity = 0;
+    }
+
+    if (square.x < -20) {
+        square.x = 1220;
+    } else if (square.x > 1220) { //if square goes off to right
+        square.x = -20;
+    }
+
+    //backdrop for each frame
+    
 }
